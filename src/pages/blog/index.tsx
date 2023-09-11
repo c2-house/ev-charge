6155a1d191a52b6b222040855e986edc2a7198b0
eunchae <kec0130@gmail.com>
2023-09-11 14:17:22 +0900

    Add: get posts from db

--- src/pages/blog/index.tsx
+++ src/pages/blog/index.tsx
@@ -1,8 +1,13 @@
+import { GetStaticProps, InferGetStaticPropsType } from 'next';
+import { List } from '@chakra-ui/react';
+
+import { Post } from '@/types/supabase';
+import { getAllPosts } from '@/services/blog';
 import generateRssFeed from '@/utils/rss';
 import Metadata from '@/components/Metadata';
-import PostList from '@/components/Blog/PostList';
+import PostListItem from '@/components/Blog/PostListItem';
 
-const Blog = () => {
+const Blog = ({ posts }: InferGetStaticPropsType<typeof getStaticProps>) => {
   return (
     <>
       <Metadata
@@ -11,16 +16,30 @@ const Blog = () => {
         keywords='전기차 블로그, 전기차 정보, 전기차 팁'
         url='/blog'
       />
-      <PostList />
+
+      <List>
+        {posts.map(({ id, title, description, slug, image_url }) => (
+          <PostListItem
+            key={id}
+            title={title}
+            description={description}
+            slug={slug}
+            imgSrc={image_url}
+          />
+        ))}
+      </List>
     </>
   );
 };
 
-export const getStaticProps = async () => {
+export const getStaticProps: GetStaticProps<{ posts: Post[] }> = async () => {
+  const { posts } = await getAllPosts();
   generateRssFeed();
 
   return {
-    props: {},
+    props: {
+      posts,
+    },
   };
 };
 
