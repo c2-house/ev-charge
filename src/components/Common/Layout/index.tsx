8b867c91c3b4cfec88d868b08532491851fd28a8
eunchae <kec0130@gmail.com>
2023-09-18 22:40:15 +0900

    Fix: footer style

--- src/components/Common/Layout/index.tsx
+++ src/components/Common/Layout/index.tsx
@@ -1,30 +1,20 @@
-import { useRouter } from "next/router";
-import { Text, Container, useTheme, Divider } from "@chakra-ui/react";
-import Navigation from "./Navigation";
+import { useRouter } from 'next/router';
+import { Container, useTheme } from '@chakra-ui/react';
+import Navigation from './Navigation';
+import Footer from './Footer';
 
 const Layout = ({ children }: { children: React.ReactNode }) => {
   const theme = useTheme();
   const router = useRouter();
-  const mapPage = router.asPath === "/";
+  const mapPage = router.asPath === '/';
 
   return (
     <>
       <Navigation />
-      <Container
-        as="main"
-        maxW="container.lg"
-        p={mapPage ? 0 : [4, 6]}
-        mt={theme.sizes.navHeight}
-        mb="env(safe-area-inset-bottom)"
-      >
+      <Container as='main' maxW='container.lg' p={mapPage ? 0 : [4, 6]} mt={theme.sizes.navHeight}>
         {children}
       </Container>
-      <Container as="footer" textAlign="center" maxW="container.lg" py={2}>
-        <Divider />
-        <Text color="gray.500" my={2}>
-          &copy; 전기차G | 문의: chaechae.couple@gmail.com
-        </Text>
-      </Container>
+      {!mapPage && <Footer />}
     </>
   );
 };
