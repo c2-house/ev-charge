8579beb0c75917ea7b575115f3b05ca0e9444cdb
EunchaeKo <kec0130@gmail.com>
2025-08-12 16:01:28 +0900

    Update site verification

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -27,8 +27,12 @@ export default function App({ Component, pageProps }: AppProps) {
           <meta name='theme-color' content='#ffffff' />
           <meta
             name='google-site-verification'
-            content='yAPCOjrjXkneaFLU21zJVUZT_mDKpTL3BM8QPsHgvN0'
+            content='kCxJGsTC0g9IhzyOexCiJ0lbrB0hvAmHDz4kfv2sunY'
           />
+          {/* <meta
+            name='google-site-verification'
+            content='yAPCOjrjXkneaFLU21zJVUZT_mDKpTL3BM8QPsHgvN0'
+          /> */}
           <meta name='naver-site-verification' content='b3044df87ae5838592940b7d467f77932ccd4063' />
         </Head>
         <div className={pretendard.className}>
